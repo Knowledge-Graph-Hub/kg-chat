1415624bf13e3f2ef93cd57f99c9fc70dda1b8b4
Harshad Hegde <hegdehb@gmail.com>
2024-07-10 13:31:20 -0500

    nods and edges path corrected

--- src/kg_chat/constants.py
+++ src/kg_chat/constants.py
@@ -5,10 +5,10 @@ from pathlib import Path
 PWD = Path(__file__).parent
 DATA_DIR = PWD / "data"
 GRAPH_OUTPUT_DIR = PWD / "graph_output"
-# NODES_FILE = DATA_DIR / "nodes.tsv"
-# EDGES_FILE = DATA_DIR / "edges.tsv"
-NODES_FILE = DATA_DIR / "merged-kg_nodes.tsv"
-EDGES_FILE = DATA_DIR / "merged-kg_edges.tsv"
+NODES_FILE = DATA_DIR / "nodes.tsv"
+EDGES_FILE = DATA_DIR / "edges.tsv"
+# NODES_FILE = DATA_DIR / "merged-kg_nodes.tsv"
+# EDGES_FILE = DATA_DIR / "merged-kg_edges.tsv"
 
 NEO4J_BATCH_SIZE = 2000  # Adjust the batch size as needed
 NEO4J_USERNAME = "neo4j"
