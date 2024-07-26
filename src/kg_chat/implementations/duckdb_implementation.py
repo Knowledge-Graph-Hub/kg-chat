5de5907c61d21773c43a31f06516d5ce43468111
Harshad Hegde <hegdehb@gmail.com>
2024-07-26 15:39:29 -0500

    formatted

--- src/kg_chat/implementations/duckdb_implementation.py
+++ src/kg_chat/implementations/duckdb_implementation.py
@@ -15,8 +15,6 @@ from kg_chat.config.llm_config import LLMConfig
 from kg_chat.interface.database_interface import DatabaseInterface
 from kg_chat.utils import llm_factory, structure_query
 
-from langchain.callbacks.tracers import ConsoleCallbackHandler
-
 
 class DuckDBImplementation(DatabaseInterface):
     """Implementation of the DatabaseInterface for DuckDB."""
@@ -36,12 +34,13 @@ class DuckDBImplementation(DatabaseInterface):
         self.db = SQLDatabase(self.engine, view_support=True)
         self.toolkit = SQLDatabaseToolkit(db=self.db, llm=self.llm)
         self.agent = create_sql_agent(
-            llm=self.llm, verbose=True,
+            llm=self.llm,
+            verbose=True,
             toolkit=self.toolkit,
             agent_executor_kwargs=dict(
-                    return_intermediate_steps=True,
-                    handle_parsing_errors=True,
-                ),
+                return_intermediate_steps=True,
+                handle_parsing_errors=True,
+            ),
             # config={"callbacks":[ConsoleCallbackHandler()]}
         )
 
